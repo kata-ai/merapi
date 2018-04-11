17a616a8ca566390328ea822117e881d31ebd209
Ikmal Syifai <ikmal.s.16@gmail.com>
2018-04-11 14:41:23 +0700

    Fix component registration (#9)

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ class Container extends Component.mixin(AsyncEmitter) {
     register(name, type, options) {
         if (typeof options == "boolean")
             return this.injector.register(name, type, options);
-        if (typeof type == "function" || type == "object")
+        if (typeof type == "function" || typeof type == "object")
             return this.injector.register(name, type);
 
         return this.injector.register(name, this.loadComponent(name, type, options));
